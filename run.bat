e3a8db8276afdf0b3c56120155c5715b5abaa4d2
1BASiC <ayhemgamez@gmail.com>
2026-03-10 00:34:21 +0300

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+title Moderation Bot - By DraGoN3DD
+color 0a
+echo =========================================
+echo      Starting Moderation Bot...
+echo      Developer: DraGoN3DD
+echo =========================================
+python main.py
+pause
